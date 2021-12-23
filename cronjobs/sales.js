b03a7515bc528717e15ff261257d0ec514bcea1c
mihirvaja <mihirvaja@hotmail.co.uk>
2021-12-23 12:25:33 +0000

    Update sales.js

--- cronjobs/sales.js
+++ cronjobs/sales.js
@@ -9,7 +9,7 @@ module.exports = {
   name: 'sales',
   description: 'sales bot',
   interval: 30000,
-  enabled: process.env.DISCORD_SALES_CHANNEL_ID != null,
+  enabled: process.env.salesChannelID != null,
   async execute(client) {
     if (lastTimestamp == null) {
       lastTimestamp = Math.floor(Date.now()/1000) - 120;
@@ -24,13 +24,13 @@ module.exports = {
     let offset = 0;
     let settings = { 
       method: "GET",
-      headers: process.env.OPEN_SEA_API_KEY == null ? {} : {
-        "X-API-KEY": process.env.OPEN_SEA_API_KEY
+      headers: process.env.apiKey == null ? {} : {
+        "X-API-KEY": process.env.apiKey
       }
     };
     while(1)
     {
-      let url = `${openseaEventsUrl}?collection_slug=${process.env.OPEN_SEA_COLLECTION_NAME}&event_type=successful&only_opensea=false&offset=${offset}&limit=50&occurred_after=${lastTimestamp}&occurred_before=${newTimestamp}`;
+      let url = `${openseaEventsUrl}?collection_slug=${process.env.osCollectionName}&event_type=successful&only_opensea=false&offset=${offset}&limit=50&occurred_after=${lastTimestamp}&occurred_before=${newTimestamp}`;
       try {
         var res = await fetch(url, settings);
         if (res.status != 200) {
@@ -60,7 +60,7 @@ module.exports = {
               .addField("From", `[${event.seller.user?.username || event.seller.address.slice(0,8)}](https://etherscan.io/address/${event.seller.address})`, true)
               .addField("To", `[${event.winner_account.user?.username || event.winner_account.address.slice(0,8)}](https://etherscan.io/address/${event.winner_account.address})`, true);
 
-            client.channels.fetch(process.env.DISCORD_SALES_CHANNEL_ID)
+            client.channels.fetch(process.env.salesChannelID)
               .then(channel => {
                 channel.send(embedMsg);
               })
